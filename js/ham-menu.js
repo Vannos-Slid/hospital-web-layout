0de215d7f84c4bdf42944598780ed1558f71f8d7
Vannos-Slid <ivan.kush05@gmail.com>
2025-08-04 23:00:22 +0300

    update hamburger menu functional

--- js/ham-menu.js
+++ js/ham-menu.js
@@ -1,8 +1,27 @@
 const hamMenu = document.querySelector('.ham-menu')
-
 const offScreenMenu = document.querySelector('.off-screen-menu')
+const overlay = document.querySelector('#overlay')
 
-hamMenu.addEventListener('click', () => {
+const media = window.matchMedia("(width < 800px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+hamMenu.addEventListener('click', () => toggleNavbar())
+
+function updateNavbar(e) {
+    const isMobile = e.matches;
+    console.log(isMobile)
+    // if (isMobile) {
+    //     offScreenMenu.setAttribute("inert", '')
+    // } else {
+    //     offScreenMenu.removeAttribute("inert")
+    // }
+}
+
+function toggleNavbar() {
     hamMenu.classList.toggle('active')
     offScreenMenu.classList.toggle('active')
-})
+    overlay.classList.toggle('active')
+}
+
+updateNavbar(media)
