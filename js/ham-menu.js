cac87c4bab8dcf8ce90ea4f314dbaaae101e85c5
Vannos-Slid <ivan.kush05@gmail.com>
2025-08-05 23:11:50 +0300

    updated navbar and ham-menu functional

--- js/ham-menu.js
+++ js/ham-menu.js
@@ -1,27 +1,45 @@
-const hamMenu = document.querySelector('.ham-menu')
-const offScreenMenu = document.querySelector('.off-screen-menu')
+const hamMenus = document.querySelectorAll('.ham-menu')
+const navbar = document.querySelector('.header-section-nav')
+const closeButtonWrapper = document.querySelector('.close-button-wrapper')
 const overlay = document.querySelector('#overlay')
 
 const media = window.matchMedia("(width < 800px)")
 
 media.addEventListener('change', (e) => updateNavbar(e))
 
-hamMenu.addEventListener('click', () => toggleNavbar())
-
 function updateNavbar(e) {
     const isMobile = e.matches;
     console.log(isMobile)
-    // if (isMobile) {
-    //     offScreenMenu.setAttribute("inert", '')
-    // } else {
-    //     offScreenMenu.removeAttribute("inert")
-    // }
+    if (isMobile) {
+        navbar.setAttribute("inert", '')
+    } else {
+        navbar.removeAttribute("inert")
+    }
 }
 
-function toggleNavbar() {
-    hamMenu.classList.toggle('active')
-    offScreenMenu.classList.toggle('active')
-    overlay.classList.toggle('active')
+function openSidebar() {
+    hamMenus.forEach(menu => {
+        menu.classList.add('active')
+    })
+    closeButtonWrapper.classList.add('active')
+    navbar.classList.add('active')
+    navbar.removeAttribute('inert')
 }
+
+function closeSidebar() {
+    hamMenus.forEach(menu => {
+        menu.classList.remove('active')
+    })
+    closeButtonWrapper.classList.remove('active')
+    navbar.classList.remove('active')
+    navbar.setAttribute('inert', '')
+}
+
+const navLinks = document.querySelectorAll('nav a')
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        closeSidebar();
+    })
+})
 
 updateNavbar(media)
